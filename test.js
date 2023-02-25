7a63508824af65ff79c93cc992ba1762d64f65f4
hydricAcid <shadowcreeper255@gmail.com>
2023-02-25 23:10:10 +1100

    change 3

--- test.js
+++ test.js
@@ -1,8 +1,3 @@
-let images = document.querySelectorAll('div[class*="Avatar__Avatar___gE5kx Avatar__staff___4gVLs MessageList__avatar___2wxyb "]')
-
-for (i = 0; i < images.length; i++) {
-    images[i].src = browser.runtime.getURL("images/kitten.jpg")
-}
 
 var cssFile = chrome.runtime.getURL("injected.css");
 var fileref = document.createElement("link");
@@ -11,3 +6,4 @@ fileref.setAttribute("type", "text/css");
 fileref.setAttribute("href", cssFile);
 document.head.appendChild(fileref);
 document.getElementsByTagName("html")[0].appendChild(fileref);
+
