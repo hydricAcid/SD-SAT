59c2b0ad1a7de3b71fc0db1cb24e4ffd1921b10d
hydricAcid <shadowcreeper255@gmail.com>
2023-02-28 15:11:26 +1100

    change 5

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 
 var cssFile = chrome.runtime.getURL("injected.css");
-var fileref = document.createElement("link");
-fileref.setAttribute("rel", "stylesheet");
-fileref.setAttribute("type", "text/css");
-fileref.setAttribute("href", cssFile);
-document.head.appendChild(fileref);
-document.getElementsByTagName("html")[0].appendChild(fileref);
+var ref = document.createElement("link");
+ref.setAttribute("rel", "stylesheet");
+ref.setAttribute("type", "text/css");
+ref.setAttribute("href", cssFile);
+document.head.appendChild(ref);
+document.getElementsByTagName("html")[0].appendChild(ref);
 
 
 document.getElementsByClassName("li.item.hasChildren").element.setAttribute(onclick, "");
